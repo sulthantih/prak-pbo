6c3d238acb59e5cd0072ed71962d2b7a20cf3928
Sulthan Fatih Putradewa <114857938+sulthantih@users.noreply.github.com>
2024-03-07 23:35:39 +0700

    Update 122140183_Sulthan Fatih P_Prak2_soal1.py

--- prak2/122140183_Sulthan Fatih P_Prak2_soal1.py
+++ prak2/122140183_Sulthan Fatih P_Prak2_soal1.py
@@ -1,45 +1,68 @@
 class Mahasiswa:
     def __init__(self, nim, nama, angkatan, isMahasiswa=True):
+        print("Sebuah objek Mahasiswa telah dibuat.")
         self.__nim = nim
         self.__nama = nama
-        self.angkatan = angkatan
-        self.isMahasiswa = isMahasiswa
-
-    @property
-    def nama(self):
-        return self.__nama
-
-    @nama.setter
-    def nama(self, value):
-        self.__nama = value
-
-    @property
-    def nim(self):
+        self.__angkatan = angkatan
+        self.__isMahasiswa = isMahasiswa
+    
+    def getNIM(self):
         return self.__nim
+    
+    def setNIM(self, nim):
+        self.__nim = nim
+    
+    def getNama(self):
+        return self.__nama
+    
+    def setNama(self, nama):
+        self.__nama = nama
+    
+    def getAngkatan(self):
+        return self.__angkatan
+    
+    def setAngkatan(self, angkatan):
+        self.__angkatan = angkatan
+    
+    def getIsMahasiswa(self):
+        return self.__isMahasiswa
+    
+    def method1(self):
+        return self.__nama.upper()
+    
+    def method2(self):
+        return f"{self.__nama} adalah mahasiswa angkatan {self.__angkatan}"
+    
+    def method3(self, value):
+        if value > 0:
+            return f"{self.__nama} memiliki nilai positif"
+        else:
+            return f"{self.__nama} memiliki nilai negatif"
+    
+    def __init__(self, nim, nama, angkatan, isMahasiswa=True):
+        print("Sebuah objek Mahasiswa telah dibuat.")
+        self.__nim = nim
+        self.__nama = nama
+        self.__angkatan = angkatan
+        self.__isMahasiswa = isMahasiswa
+    
+    def __del__(self):
+        print(f"Objek Mahasiswa dengan nama {self.__nama} telah dihapus.")
+        
+    @staticmethod
+    def info():
+        return "Ini adalah kelas Mahasiswa"
 
-    @nim.setter
-    def nim(self, value):
-        self.__nim = value
 
-    def method_1(self):
-        return f"Method 1 dijalankan oleh {self.nama}"
+mahasiswa1 = Mahasiswa("122140183", "Asep Bensin", 2022)
+mahasiswa2 = Mahasiswa("081244444", "Mamank Kesbor", 2023, False)
 
-    def method_2(self, parameter):
-        return f"Method 2 dijalankan dengan parameter: {parameter}"
+print(mahasiswa1.method1())
+print(mahasiswa1.method2())
+print(mahasiswa1.method3(10))
 
-    def method_3(self):
-        return f"Method 3 dijalankan oleh {self.nama} dengan NIM {self.nim}"
+print(mahasiswa2.method1())
+print(mahasiswa2.method2())
+print(mahasiswa2.method3(-5))
 
-
-
-mahasiswa1 = Mahasiswa("122140183", "Sulthan Standing", 2022)
-
-
-mahasiswa2 = Mahasiswa("122141231", "Mamank Kesbor", 2023)
-
-
-print(mahasiswa1.method_1())
-print(mahasiswa1.method_2("parameter"))
-
-print(mahasiswa2.method_3())
-print(mahasiswa2.method_2("parameter lain"))
+print(Mahasiswa.info())
